da2c60db85ef52ffdb52f8b6b0e26abc1110166a
Pranesh04092003 <139009385+Pranesh04092003@users.noreply.github.com>
2023-10-31 15:27:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+const video = document.getElementById('qr-video');
+const canvas = document.getElementById('qr-canvas');
+const result = document.getElementById('qr-result');
+const scanButton = document.getElementById('scan-qr-button');
+
+let scanning = false;
+
+// Add a click event listener to the "Scan QR Code" button
+scanButton.addEventListener('click', function () {
+    if (!scanning) {
+        startScanning();
+    } else {
+        stopScanning();
+    }
+});
+
+function startScanning() {
+    scanning = true;
+    scanButton.innerText = 'Stop Scanning';
+
+    // Use the getUserMedia API to access the camera
+    navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then(function (stream) {
+            video.srcObject = stream;
+            video.play();
+        })
+        .catch(function (error) {
+            console.error('Error accessing the camera:', error);
+            stopScanning();
+        });
+
+    video.style.display = 'block';
+    canvas.style.display = 'none';
+    result.style.display = 'none';
+}
+
+function stopScanning() {
+    scanning = false;
+    scanButton.innerText = 'Scan QR Code';
+
+    // Stop the camera and hide the video feed
+    if (video.srcObject) {
+        video.srcObject.getTracks().forEach(function (track) {
+            track.stop();
+        });
+    }
+
+    video.style.display = 'none';
+    canvas.style.display = 'none';
+    result.style.display = 'block';
+    result.innerHTML = 'Scan a QR code';
+}
